695ea82985b3a275186be29585e0262f8dde8167
RamtinLm <ramtin82loghmani@gmail.com>
2026-01-16 11:48:34 -0500

    add lab01

---
+++ Week01/lab1.py
@@ -0,0 +1,10 @@
+# Sample Cosding Question 01 Week 01
+# Ramtin Loghmani
+
+
+
+# Question 2: defining an array
+
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print("Now showing the shop items filtred by age", userAge)
