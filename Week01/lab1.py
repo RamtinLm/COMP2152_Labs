97d8220c7b6a2bd16c501fa65cfade8263d601d0
RamtinLm <ramtin82loghmani@gmail.com>
2026-01-18 18:28:45 -0500

    final submission code

--- Week01/lab1.py
+++ Week01/lab1.py
@@ -1,10 +1,25 @@
-# Sample Cosding Question 01 Week 01
+# Sample Coding Question 01 Week 01
 # Ramtin Loghmani
 
 
 
-# Question 2: defining an array
+# Question 02 defining an array
+my_array = [1, 4, 7, 9]
 
+# Question 03 Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+# Fully-Bracketed Version of: e = a - b ** c // d + a % c
+e = (a - ((b ** c) // d)) + (a % c)
+print("Question 3 result:", e)
+
+# Question 04 Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Question 05 User Input
 userAge = int(input("Enter your age: "))
 userAge = userAge + 22
 print("Now showing the shop items filtred by age", userAge)
